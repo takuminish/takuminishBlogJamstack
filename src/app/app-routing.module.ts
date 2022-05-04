6e8322cebf2d15c0443ae239a01cc731b0a48855
takuminish <takuminv1997@gmail.com>
2022-05-05 00:50:31 +0900

    Topコンポーネントの作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopComponent } from './top/top.component';
 
-const routes: Routes = [{ path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }];
+const routes: Routes = [
+  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
+  { path: '', component: TopComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
