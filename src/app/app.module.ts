f17a73d3c0bf5ca6ef55f2b18cc0698c1a5e77db
takuminish <takuminv1997@gmail.com>
2022-05-09 23:37:21 +0900

    Headerコンポーネントの作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { HeaderComponent } from './Header/Header.component';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-  ],
+      HeaderComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
