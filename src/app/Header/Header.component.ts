0fb74c6d2dde2abf5e1a2c674a6a4f7ba7d451a9
takuminish <30888611+takuminish@users.noreply.github.com>
2022-05-11 23:50:58 +0900

    ヘッダーの作成 (#2)
    * Headerコンポーネントの作成
    
    * Bootstrap5のnavbarの設定
    
    * URLによってactiveになるように変更
    
    * navbar-brandの変更
    
    * navbar-brandのlinkの追加
    
    * idの追加

---
+++ src/app/Header/Header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Header',
+  templateUrl: './Header.component.html',
+  styleUrls: ['./Header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
+import { HeaderComponent } from './Header/Header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
