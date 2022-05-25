08c74f6e81c4e8e83465502c7e93b08bc1f8af2d
takuminish <takuminv1997@gmail.com>
2022-05-25 23:52:54 +0900

    @tailwindcss/typographyによるブログ記事のスタイル変更

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: ["./src/**/*.html", "./src/**/*.ts"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [require('@tailwindcss/typography')],
+};
