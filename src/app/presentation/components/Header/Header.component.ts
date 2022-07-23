1c8b2ced4ad9ca53c371000cef6a624f703b0e0b
takuminish <takuminv1997@gmail.com>
2022-07-23 17:04:00 +0900

    HeaderComponentのコメントの追記

--- src/app/presentation/components/Header/Header.component.ts
+++ src/app/presentation/components/Header/Header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/**
+ * ヘッダーのコンポーネント
+ */
 @Component({
   selector: 'app-Header',
   templateUrl: './Header.component.html',
   styleUrls: ['./Header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
+  /**
+   * コンストラクタ
+   */
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
