083ff316c1b9fb8d6884a6962277c0cd1caa1f60
takuminish <takuminv1997@gmail.com>
2022-07-23 16:56:54 +0900

    ProfileRepositoryImplのコメントを追記

--- src/app/infra/repositories/profilerepository/profile.repository.ts
+++ src/app/infra/repositories/profilerepository/profile.repository.ts
@@ -4,14 +4,25 @@ import { TakuminishProfile } from 'src/app/domain/model/resource-model/takuminis
 import { ProfileRepository } from 'src/app/domain/repositories/profilerepository/profile.repository';
 import { client } from 'src/environments/environment.api'
 
+/**
+ * プロフィール情報を取得するRepository
+ * MicroCMSのAPIを利用する
+ */
 @Injectable({
     providedIn: 'root'
 })
 export class ProfileRepositoryImpl extends ProfileRepository {
+    /**
+     * コンストラクタ
+     */
     constructor() {
         super()
     }
 
+    /**
+     * プロフィール情報をMicroCMSのAPIから取得する
+     * @returns Observeble<TakuminishProfile> プロフィール情報
+     */
     fetchProfile(): Observable<TakuminishProfile> {
         return from<Promise<TakuminishProfile>>(client.get<TakuminishProfile>({
             endpoint: 'profile',
