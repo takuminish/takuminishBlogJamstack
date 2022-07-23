37098042b02eb1fa6af1f8464d0e34f9b43da058
takuminish <takuminv1997@gmail.com>
2022-07-23 16:33:45 +0900

    フォルダ構成の変更

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScullyLibModule } from '@scullyio/ng-lib';
-import { HeaderComponent } from './Header/Header.component';
-import { BlogsComponent } from './blogs/blogs.component';
-import { SharedModule } from './shared/Shared.module';
+import { HeaderComponent } from './presentation/components/Header/Header.component';
+import { BlogsComponent } from './presentation/components/blogs/blogs.component';
+import { SharedModule } from './presentation/components/shared/Shared.module';
 
 @NgModule({
   declarations: [
