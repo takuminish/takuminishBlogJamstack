39d875ed0622ebae96f3140d30af1f1969a51f5a
takuminish <takuminv1997@gmail.com>
2022-07-24 23:53:15 +0900

    topページとblogページでscriptタグを除去するように変更

--- scully.takuminishBlogJamstack.config.ts
+++ scully.takuminishBlogJamstack.config.ts
@@ -1,19 +1,31 @@
 import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
 import { baseHrefRewrite } from '@scullyio/scully-plugin-base-href-rewrite';
+import { removeScripts, RemoveScriptsConfig } from '@scullyio/scully-plugin-remove-scripts';
 
-const defaultPostRenderers = ['seoHrefOptimise', baseHrefRewrite];
+const defaultPostRenderers = ['seoHrefOptimise', baseHrefRewrite, removeScripts];
 setPluginConfig(baseHrefRewrite, { href: '/takuminishBlogJamstack/' });
-
+setPluginConfig<RemoveScriptsConfig>(removeScripts, {
+  keepTransferstate: false,
+  keepAttributes: []
+});
 
 export const config: ScullyConfig = {
-  defaultPostRenderers,
   projectRoot: "./src",
   projectName: "takuminishBlogJamstack",
   spsModulePath: 'YOUR OWN MODULE PATH HERE',
   outDir: './dist/static',
   routes: {
+    '/home': {
+      type: 'default',
+      postRenderers: [removeScripts],
+    },
+    '/': {
+      type: 'default',
+      postRenderers: [removeScripts],
+    },
     '/blog/:slug': {
       type: 'contentFolder',
+      postRenderers: [removeScripts],
       slug: {
         folder: "./blog"
       }
