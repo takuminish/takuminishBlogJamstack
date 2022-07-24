136ee2c18cbfee8a5cf9b9b51e7d1cbf5fcf9f64
takuminish <takuminv1997@gmail.com>
2022-07-24 15:17:31 +0900

    スキル一覧をフレームワークと言語で分離

--- src/app/presentation/components/top/Skill/Skill.component.ts
+++ src/app/presentation/components/top/Skill/Skill.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Skill } from 'src/app/domain/model/resource-model/skill.model';
+
+const CATEGORY = {
+  language: '言語',
+  framework: 'フレームワーク'
+};
 
 /**
  * スキル情報のコンポーネント
@@ -9,7 +14,13 @@ import { Skill } from 'src/app/domain/model/resource-model/skill.model';
   templateUrl: './Skill.component.html',
   styleUrls: ['./Skill.component.scss']
 })
-export class SkillComponent {
+export class SkillComponent implements OnChanges {
+
+  /** フレームワーク一覧 */
+  public frameworkSkills: Skill[] = [];
+
+  /** 言語一覧 */
+  public languageSkills: Skill[] = [];
 
   /** スキル一覧 */
   @Input() skills: Skill[] = [];
@@ -18,5 +29,15 @@ export class SkillComponent {
    * コンストラクタ
    */
   constructor() { }
+
+  /**
+ * licensesが変更された場合に発火
+ * @param changes 
+ */
+  ngOnChanges(changes: SimpleChanges): void {
+    // カテゴリは配列で取得されるため、先頭のみを使用する
+    this.frameworkSkills = this.skills.filter(s => s.category[0] === CATEGORY.framework).sort()
+    this.languageSkills = this.skills.filter(s => s.category[0] === CATEGORY.language).sort();
+  }
 
 }
